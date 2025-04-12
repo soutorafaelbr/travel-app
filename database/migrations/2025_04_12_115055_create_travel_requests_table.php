9e3f8f512cdd3001304263ce2a991435b929131f
Rafa Souto <souto.rafaelb@gmail.com>
2025-04-12 08:57:13 -0300

    feat: travel request scaffold

---
+++ database/migrations/2025_04_12_115055_create_travel_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travel_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['requested', 'approved', 'canceled'])->default('requested');
+            $table->string('destination');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_requests');
+    }
+};
